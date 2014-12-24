9b283a8baaeaa67072d0055d6a1e89cf41330d2f
v11 <442629928@qq.com>
2014-12-24 09:46:38 +0800

    1
    1

--- imoney-web/src/main/java/com/imoney/web/WebRootConfiguration.java
+++ imoney-web/src/main/java/com/imoney/web/WebRootConfiguration.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
+import com.imoney.mongodb.MongoBeansConfiguration;
+import com.imoney.mongodb.MongoConfiguration;
+import com.imoney.mongodb.MongoRootConfiguration;
+
 
 @Configuration
-@Import({WebAppConfiguration.class})
+@Import({WebAppConfiguration.class,MongoBeansConfiguration.class,MongoConfiguration.class})
 @ImportResource({"classpath:applicationContext-shiro.xml"})
 @ComponentScan({"com.imoney.web","com.imoney.mongodb"})
 public class WebRootConfiguration {
--- imoney-web/src/test/java/com/imoney/web/service/account/UserLoginTest.java
+++ imoney-web/src/test/java/com/imoney/web/service/account/UserLoginTest.java
@@ -2,15 +2,16 @@ package com.imoney.web.service.account;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import com.imoney.mongodb.MongoRootConfiguration;
+import com.imoney.web.WebRootConfiguration;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {MongoRootConfiguration.class},
-						locations = { "/applicationContext-shiro.xml" })
+@ContextConfiguration(classes = {WebRootConfiguration.class})
 public class UserLoginTest {
 	
 }
